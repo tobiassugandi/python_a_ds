cea7eb3669a95d3242f6e85664e9a70b50ad5ea7
Tobias Sugandi <sugandi.tobias@gmail.com>
2024-11-17 16:07:09 +0100

    create minimal empty class, def root None, def len dunder

---
+++ tdd/binary_search_tree.py
@@ -0,0 +1,7 @@
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
--- tdd/test_binary_search_tree.py
+++ tdd/test_binary_search_tree.py
@@ -1,1 +1,12 @@
-t
+import unittest
+from binary_search_tree import BinarySearchTree
+
+class TestBInarySearchTree(unittest.TestCase):
+    def test_empty_tree(self):
+        bst = BinarySearchTree()
+        self.assertIsNone(bst.root)
+        self.assertEqual(len(bst), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
